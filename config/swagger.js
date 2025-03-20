da4a80fb3bee7c2ed5e78192cc2898d06ce7898c
unknown <aeledeisy5@gmail.com>
2025-03-20 06:17:16 +0200

    new commit

--- config/swagger.js
+++ config/swagger.js
@@ -1,6 +1,6 @@
-const swaggerUi = require("swagger-ui-express");
-const swaggerJsDoc = require("swagger-jsdoc");
-const express = require("express");
+import { serve, setup } from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+import express from "express";
 
 const app = express();
 
@@ -23,6 +23,6 @@ const swaggerOptions = {
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 // Expose Swagger UI as an API route
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api-docs", serve, setup(swaggerDocs));
 
-module.exports = app;
+export default app;
